789cc99789c9b67074e77551043d922805831adc
Nono1905 <nono1905@gmail.com>
2016-09-29 15:58:29 +0200

    Ajouts de la classe partie et modification du main

---
+++ TrafficRacer/src/Partie.cpp
@@ -0,0 +1,62 @@
+#include <SDL2/SDL.h>
+#include "Partie.h"
+#include "Header.h"
+
+
+Partie::Partie()
+{
+    jouer = true;
+}
+
+Partie::~Partie()
+{
+    //dtor
+}
+
+void Partie::gestion_evenements()
+{
+    SDL_Event event;
+    while( SDL_PollEvent( &event ) != 0 )
+    {
+        //Fermer la fenêtre
+        if (event.type == SDL_QUIT)
+        {
+            jouer = false;
+
+        }
+        // Les touches
+        else if( event.type == SDL_KEYDOWN )
+        {
+            switch( event.key.keysym.sym )
+            {
+            case SDLK_UP:
+                    break;
+
+            case SDLK_DOWN:
+                break;
+
+            case SDLK_LEFT:
+                break;
+
+            case SDLK_RIGHT:
+                break;
+
+            default:
+                break;
+            }
+        }
+
+    }
+}
+
+void Partie::afficher()
+{
+    SDL_SetRenderDrawColor(pRenderer, 00, 255, 00, 255);
+    SDL_RenderClear(pRenderer);
+    SDL_RenderPresent(pRenderer);
+}
+
+bool Partie::continuer_partie()
+{
+    return jouer;
+}
